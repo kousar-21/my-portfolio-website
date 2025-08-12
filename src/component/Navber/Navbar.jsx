486db5e1258e8bfef1333657acb51bb98dc128f2
kousar-21 <kousarahammed5@gmail.com>
2025-08-12 20:33:36 +0600

    Little modify in the navbar

--- src/component/Navber/Navbar.jsx
+++ src/component/Navber/Navbar.jsx
@@ -14,13 +14,6 @@ const Navbar = () => {
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                             <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
                             <li><a>Item 3</a></li>
                         </ul>
                     </div>
@@ -31,20 +24,11 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn">Resume</a>
                 </div>
             </div>
         </div>
